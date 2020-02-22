06135d766416ed62b887885c28ca5ad1e51ca891
Prasun Thapliyal <prasun.thapliyal@gmail.com>
2020-02-22 18:47:55 +0530

    How to use NLog in ASP.NET Core Web Application

---
+++ Services/NLogTest/NLogTest/Controllers/WeatherForecastController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NLog;
+
+namespace NLogTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+
+            var loggerTemp = LogManager.GetLogger("foo");
+            loggerTemp.Error("This is foo");
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            _logger.LogInformation("This is a info log");
+            _logger.LogError("This is a error log");
+            _logger.LogDebug("This is a debug log");
+            _logger.LogTrace("This is a trace log");
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ Services/NLogTest/NLogTest/ReadMe.txt
@@ -0,0 +1,42 @@
+﻿22 Feb 2020
+===========
+
+Try NLog
+
+ref: https://github.com/NLog/NLog/wiki/Getting-started-with-ASP.NET-Core-3
+
+(#) Visual Studio 2019
+	-> New Project -> ASP.NET Core Web Application
+	-> API -> No Docker/No Auth/No HTTPS
+
+Note: This comes with a default logger, by class Microsoft.Extensions.Logging.Logger
+However, we want to try NLog's logger, i.e. NLog.Logger
+
+(#) Nuget -> Install -> NLog.Web.AspNetCore
+	Nuget -> Install -> NLog
+
+(#) Create nlog.config (lowercase all) file in the root of your project.
+	File Properties -> Ensure 
+	-> Build Action = Content (default)
+	-> Copy to output folder = Copy if newer (default)
+
+(#) In Program.cs, add UseNLog to HostBuilder like so:
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                })
+            .UseNLog(); // You need to add: using NLog.Web;
+    If you miss adding this, your program will continue using the Microsoft logger
+    You can also obtain a logger by calling var loggerTemp = LogManager.GetLogger("foo");
+    And this will also return you a NLog.Logger (LogManager comes from NLog DLL)
+
+(#) After adding UseNLog(), the ASP.NET DI system will return NLog.Logger when you ask for ILogger<T>
+
+That's it !
+
+Explore more about the nlog.config file on Google .. by and large there are targets and rules
+
+
+
